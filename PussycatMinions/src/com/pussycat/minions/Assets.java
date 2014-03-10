a6980d6d97f73b89cb452cf1dc3d734c48755593
Mikael Zackrisson <micke.zackrisson@gmail.com>
2014-03-10 09:41:47 +0100

    button Class added and implemented in Main Menu

--- PussycatMinions/src/com/pussycat/minions/Assets.java
+++ PussycatMinions/src/com/pussycat/minions/Assets.java
@@ -6,5 +6,9 @@ import com.pussycat.framework.Sound;
 public class Assets {
     
     public static Image ball;
+    public static Image button_player;
+    public static Image button_player_pressed;
+    public static Image button_middle;
+    public static Image button_middle_pressed;
     public static Sound click;
 }
--- PussycatMinions/src/com/pussycat/minions/MainMenuScreen.java
+++ PussycatMinions/src/com/pussycat/minions/MainMenuScreen.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 
 import com.pussycat.framework.Game;
 import com.pussycat.framework.Graphics;
@@ -13,6 +14,8 @@ import com.pussycat.framework.Input.TouchEvent;
 
 public class MainMenuScreen extends Screen {
 	
+	Button playerButton;
+	Button middleButton;
 	Paint paint;
 	
     public MainMenuScreen(Game game) {
@@ -24,6 +27,10 @@ public class MainMenuScreen extends Screen {
 		paint.setTextAlign(Paint.Align.CENTER);
 		paint.setAntiAlias(true);
 		paint.setColor(Color.WHITE);
+		
+		playerButton = new Button(Assets.button_player, Assets.button_player_pressed, PussycatMinions.getScreenWidth()/2 - Assets.button_middle.getWidth()/2, PussycatMinions.getScreenHeight()/2-100);
+		middleButton = new Button(Assets.button_middle, Assets.button_middle_pressed, PussycatMinions.getScreenWidth()/2 - Assets.button_middle.getWidth()/2, PussycatMinions.getScreenHeight()/2+100);
+        
     }
     
     @Override
@@ -34,12 +41,40 @@ public class MainMenuScreen extends Screen {
         int len = touchEvents.size();
         for (int i = 0; i < len; i++) {
             TouchEvent event = touchEvents.get(i);
+            
+            if(event.type == TouchEvent.TOUCH_DOWN){
+            	if(playerButton.inBounds(event.x, event.y)){
+            		playerButton.setPressed(true);
+            	}
+            	if(middleButton.inBounds(event.x, event.y)){
+            		middleButton.setPressed(true);
+            	}
+            }
+            
+            if(event.type == TouchEvent.TOUCH_DRAGGED){
+            	if(!playerButton.inBounds(event.x, event.y)){
+            		playerButton.setPressed(false);
+            	}
+            	if(!middleButton.inBounds(event.x, event.y)){
+            		middleButton.setPressed(false);
+            	}
+            }
+            
             if (event.type == TouchEvent.TOUCH_UP) {
-            	//START GAME
-            	if(inBounds(event, 0,0, PussycatMinions.getScreenWidth(), PussycatMinions.getScreenHeight()/2))
+            	playerButton.setPressed(false);
+            	middleButton.setPressed(false);
+            	
+            	if(playerButton.inBounds(event.x, event.y)){
             		game.setScreen(new GameScreen(game));
-            	if(inBounds(event, 0,PussycatMinions.getScreenHeight()/2, PussycatMinions.getScreenWidth(), PussycatMinions.getScreenHeight()))
+            	}
+            	if(middleButton.inBounds(event.x, event.y)){
             		game.setScreen(new GameScreenMiddle(game));
+            	}
+            	//START GAME
+//            	if(inBounds(event, 0,0, PussycatMinions.getScreenWidth(), PussycatMinions.getScreenHeight()/2))
+//            		game.setScreen(new GameScreen(game));
+//            	if(inBounds(event, 0,PussycatMinions.getScreenHeight()/2, PussycatMinions.getScreenWidth(), PussycatMinions.getScreenHeight()))
+//            		game.setScreen(new GameScreenMiddle(game));
             }
         }
     }
@@ -57,8 +92,15 @@ public class MainMenuScreen extends Screen {
     @Override
     public void paint(float deltaTime) {
         Graphics g = game.getGraphics();
-        g.drawString("Tap anywhere to play.",
-                640, 300, paint);
+        
+        drawUI();
+    }
+    
+    private void drawUI() {
+        Graphics g = game.getGraphics();
+        
+        playerButton.drawButton(g);
+        middleButton.drawButton(g);
     }
 
 
