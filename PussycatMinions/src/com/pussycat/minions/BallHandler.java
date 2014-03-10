79d173a43184d90472029990cc17fd7cf5dd748e
SebbPiwell <sebastian.piwell@gmail.com>
2014-03-10 17:17:50 +0100

    To not create two TCPClients

--- PussycatMinions/src/com/pussycat/minions/BallHandler.java
+++ PussycatMinions/src/com/pussycat/minions/BallHandler.java
@@ -33,8 +33,8 @@ public class BallHandler {
 		
 		balls = new ArrayList<Ball>();
 		
-		Looper.prepare();
-		new connectTask().execute("");
+		//Looper.prepare();
+		//new connectTask().execute("");
 		
 		Log.d("Debug Pussycat", "ADDING CONNECTION");
 		
